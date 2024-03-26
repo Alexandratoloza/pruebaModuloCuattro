f78c3596dd6b1de1f9532564603192555b9e1ee2
alexandra toloza rios <toloza.ale1992@gmail.com>
2024-03-26 13:02:51 -0300

    evaluacion modulo cuatro

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ class Animal {
         this._sonido = sonido;
     }
 
-    // Getter y Setter para el nombre
+   
     get nombre() {
         return this._nombre;
     }
@@ -15,7 +15,7 @@ class Animal {
         this._nombre = nuevoNombre;
     }
 
-    // Getter y Setter para la edad
+   
     get edad() {
         return this._edad;
     }
@@ -23,7 +23,7 @@ class Animal {
         this._edad = nuevaEdad;
     }
 
-    // Getter y Setter para los comentarios
+    
     get comentarios() {
         return this._comentarios;
     }
@@ -31,7 +31,7 @@ class Animal {
         this._comentarios = nuevosComentarios;
     }
 
-    // Getter y Setter para la imagen
+    
     get imagen() {
         return this._imagen;
     }
@@ -39,7 +39,7 @@ class Animal {
         this._imagen = nuevaImagen;
     }
 
-    // Getter y Setter para el sonido
+   
     get sonido() {
         return this._sonido;
     }
@@ -49,48 +49,36 @@ class Animal {
 };
 
 
-
-// Función para cargar los datos del archivo JSON
 async function cargarDatosAnimales() {
     try {
         const response = await fetch('animales.json');
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Error al cargar los datos de los animales:', error);
         return [];
     }
 }
 
-// Función autoejecutable IIFE para cargar los datos de los animales al inicio
 (async () => {
     const data = await cargarDatosAnimales();
-    // Si se cargan los datos correctamente, puedes hacer uso de ellos aquí
-    console.log(data);
+    //console.log(data);
 })();
 
-// Función para registrar un nuevo animal
+
 async function registrarAnimal() {
-    // Obtener valores del formulario
     const nombreAnimal = document.getElementById('listaAnimal').value;
     const edadAnimal = document.getElementById('edadAnimal').value;
     const comentariosAnimal = document.getElementById('comentarios').value;
 
-    // Cargar datos de animales
+   
     const data = await cargarDatosAnimales();
-
-    // Encontrar el animal seleccionado en los datos
     const animalSeleccionado = data.find(animal => animal.name === nombreAnimal);
 
-    // Crear instancia del animal seleccionado
     if (animalSeleccionado) {
         const nuevoAnimal = new Animal(nombreAnimal, edadAnimal, comentariosAnimal, `assets/imgs/${animalSeleccionado.imagen}`, `assets/sounds/${animalSeleccionado.sonido}`);
-        // Agregar el nuevo animal a la tabla
         agregarAnimalATabla(nuevoAnimal);
-        // Mostrar imagen en la vista previa
         mostrarImagen(nuevoAnimal.imagen);
-        // Mostrar comentarios del animal
-        mostrarComentarios(nuevoAnimal.comentarios);
+      mostrarComentarios(nuevoAnimal.comentarios);
     } else {
         console.error('Animal no encontrado en los datos.');
     }
@@ -98,7 +86,6 @@ async function registrarAnimal() {
 
 }
 
-// Función para agregar un animal a la tabla
 function agregarAnimalATabla(animal) {
     const tabla = document.getElementById('registrarAnimales');
     const nuevoElemento = document.createElement('div');
@@ -119,18 +106,15 @@ function agregarAnimalATabla(animal) {
     tabla.appendChild(nuevoElemento);
 }
 
-// Función para mostrar la imagen en la vista previa
+
 function mostrarImagen(rutaImagen) {
     const preview = document.getElementById("preview");
-    // Limpiar el contenido previo de la vista previa
+   
     preview.innerHTML = "";
-    // Crear un elemento de imagen
+   
     const imagen = document.createElement("img");
-    // Establecer la ruta de la imagen como la fuente del elemento de imagen
     imagen.src = rutaImagen;
-    // Establecer clases para el estilo si es necesario
     imagen.classList.add("img-fluid");
-    // Agregar la imagen a la vista previa
     preview.appendChild(imagen);
 }
 
